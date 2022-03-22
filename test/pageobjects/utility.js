c6546d8d2cebb47d33d9b36d22ebdd20185eafe2
Nikhil Bisht <nikhilbisht96@gmail.com>
2022-03-22 20:43:08 +0530

    modified date function

--- test/pageobjects/utility.js
+++ test/pageobjects/utility.js
@@ -1,7 +1,20 @@
-class Utility{
-    async todayDate(){
-        var today= new Date()
-        return `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()}`
+class Utility {
+
+    async getDate(dayFromNow = undefined, isPastDate = undefined) {
+        var today, date;
+        today = new Date();
+        if (dayFromNow != undefined) {
+            if (isPastDate) {
+                today.setDate(today.getDate() - dayFromNow)
+            } else {
+                today.setDate(today.getDate() + dayFromNow)
+            }
+
+        }
+        date = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`
+
+        return date;
+
     }
 }
 
